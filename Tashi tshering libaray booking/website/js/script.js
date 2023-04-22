25950787cabb621a0036b93ed5b9d024877e7851
17463193 <118905694+17463193@users.noreply.github.com>
2023-04-22 11:00:17 +0600

    Add files via upload

---
+++ Tashi tshering libaray booking/website/js/script.js
@@ -0,0 +1,160 @@
+//  searchForm
+ searchForm = document.querySelector('.search-form');
+
+ document.querySelector('#search-btn').onclick=()=>{
+    searchForm.classList.toggle('active');
+ }//  searchForm
+
+ let loginForm = document.querySelector('.login-form-container');
+ document.querySelector("#login-btn").onclick = ()=>{
+    loginForm.classList.toggle('active');
+ }
+ document.querySelector("#close-login-btn").onclick = ()=>{
+    loginForm.classList.remove('active');
+ }
+ window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    if(window.scrollY > 80){
+        document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+}
+window.onload = () =>{
+    if(window.scrollY > 80){
+        document.querySelector('.header .header-2').classList.add('active');
+    }else{
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+}
+let AppointForm = document.querySelector('.appoint-form-container');
+document.querySelector("#appoint-btn").onclick = ()=>{
+  AppointForm.classList.toggle('active');
+}
+document.querySelector("#close-appoint-btn").onclick = ()=>{
+  AppointForm.classList.remove('active');
+}
+
+var swiper = new Swiper(".books-slider", {
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInterection: false,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+  var swiper = new Swiper(".featured-slider", {
+    spaceBetween: 10,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInterection: false,
+    },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      450: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+      1024: {
+        slidesPerView: 5,
+      },
+    },
+  });
+
+  var swiper = new Swiper(".arrivals-slider", {
+    spaceBetween: 10,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInterection: false,
+    },
+   
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+     
+    },
+  });
+  var swiper = new Swiper(".reviews-slider", {
+    spaceBetween: 10,
+    grabCursor:true,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInterection: false,
+    },
+   
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+     
+    },
+  });
+
+  var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 10,
+    grabCursor:true,
+    loop:true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInterection: false,
+    },
+   
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+     
+    },
+  });
